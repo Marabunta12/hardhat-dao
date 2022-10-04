adaa9bafa8c21d7fcd415c67a996090914a686d9
Bartek <s210869@sggw.edu.pl>
2022-10-04 17:47:37 +0200

    added governor and timelock contaracts

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
